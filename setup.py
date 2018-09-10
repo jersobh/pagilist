5ad63af849186cc972dfbda6080480fc369b8660
Jefferson de Andrade Santos <jersobh@users.noreply.github.com>
2018-09-10 15:44:12 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setuptools.setup(
     description="List pagination by page or limit,offset",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/pypa/sampleproject",
+    url="https://github.com/jersobh/pagilist",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
